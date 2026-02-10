8296fc80bbffcb1eb2226d3af0e82a37bdb8d84c
DucPham5 <phamduc2k5@gmail.com>
2026-02-10 17:44:58 -0600

    add model training notebooks

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     INPUT_VIDEO = "jabarismith.mp4"
     OUTPUT_VIDEO = "output_detection_pose.mp4"
     model = YOLO("models/player_detector.pt")
-    results = model.predict(INPUT_VIDEO, save = True)
+    results = model.track(INPUT_VIDEO, save = True, show_labels=True, show_conf = False)
     for box in results[0].boxes:
         print(box)
   
